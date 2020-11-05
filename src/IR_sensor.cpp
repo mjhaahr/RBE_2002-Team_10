8ad19c8dff21e425a5863e5eb80eb449605da2a2
mjhaahr <mjhaahr@wpi.edu>
2020-11-04 22:41:41 -0500

    Add Supplied Lab 2 Repo

---
+++ src/IR_sensor.cpp
@@ -0,0 +1,19 @@
+#include <Romi32U4.h>
+#include "IR_sensor.h"
+
+void IRsensor::Init(void)
+{
+    pinMode(pin_IR, INPUT);
+}
+
+float IRsensor::PrintData(void)
+{
+    Serial.println(ReadData());
+}
+
+float IRsensor::ReadData(void)
+{
+  //assignment 1.1
+  //read out and calibrate your IR sensor, to convert readouts to distance in [cm]
+  return 0;
+}
---
+++ src/Sonar_sensor.cpp
@@ -0,0 +1,20 @@
+#include <Romi32U4.h>
+#include "Sonar_sensor.h"
+
+void SonarSensor::Init(void)
+{
+    pinMode(pin_TRIG,OUTPUT);
+    pinMode(pin_ECHO, INPUT);   
+}
+
+float SonarSensor::PrintData(void)
+{
+    Serial.println(ReadData());
+}
+
+float SonarSensor::ReadData(void)
+{
+    //assignment 1.2
+    //read out and calibrate your sonar sensor, to convert readouts to distance in [cm]
+    return 0;
+}
