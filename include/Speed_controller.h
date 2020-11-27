d11f9c9b03ef7b9a52041a76f22b5988c8502175
mjhaahr <mjhaahr@wpi.edu>
2020-11-27 14:15:49 -0500

    Initial Draft of Acceleration Constrain Functionality

---
+++ include/Speed_controller.h
@@ -0,0 +1,32 @@
+#ifndef SPEED_CONTROLLER
+#define SPEED_CONTROLLER
+
+#include <Romi32U4.h>
+
+class SpeedController{
+    private:
+        const float Kp = 0.5; //Adapt the parameters until your robot moves at the speed you command it to drive
+        const float Ki = 0.1; 
+        float E_left = 0; 
+        float E_right = 0;
+        int counts = 1450; //number of counts for a 180 degree turn; you will likely have to change this
+        float error_distance = 0;
+		float maxAccel = 20.0; //mm/2^2
+		float deltaV = maxAccel * 0.050; //change in velocity from maxAccel and encoder update rate
+		int constrainAccel(int); //acceleration constrain
+    public:
+        struct constrained_acceleration {
+            float constrained_velocity_left;
+            float constrained_velocity_right;
+
+        };
+        void Init(void);
+        void Run(float, float); 
+        boolean Turn(int,int); //degrees, direction of rotation: 0->left, 1->right
+        boolean Straight(int, int); //speed, duration
+        boolean Curved(int,int,int); //speed left, speed right, duration
+        boolean MoveToPosition(float,float); //target_x, target_y
+        void Stop(void);
+};
+
+#endif
---
+++ src/Speed_controller.cpp
@@ -0,0 +1,92 @@
+#include <Romi32U4.h>
+#include "Encoders.h"
+#include  "Speed_controller.h"
+#include "Position_estimation.h"
+
+Romi32U4Motors motors;
+Encoder MagneticEncoder; 
+Position odometry;
+
+float time_track = 0;
+
+void SpeedController::Init(void){
+    MagneticEncoder.Init();
+    odometry.Init();
+}
+
+void SpeedController::Run(float target_velocity_left, float target_velocity_right){
+    if(MagneticEncoder.UpdateEncoderCounts()){
+        time_track = time_track + 50/1000.0;
+        float e_left = target_velocity_left - MagneticEncoder.ReadVelocityLeft();
+        float e_right = target_velocity_right - MagneticEncoder.ReadVelocityRight();
+        
+        E_left += e_left;
+        E_right += e_right;
+
+        float u_left = Kp*e_left + Ki*E_left;
+        float u_right = Kp*e_right + Ki*E_right;
+
+        motors.setEfforts(u_left,u_right);
+        odometry.UpdatePose(target_velocity_left,target_velocity_right);
+    }
+}
+
+boolean SpeedController::MoveToPosition(float target_x, float target_y){
+    do
+    {    
+        //assignment
+
+
+        
+    } while (error_distance >= 0.00); //define a distance criteria that lets the robot know that it reached the waypoint.
+    return 1;
+}
+
+boolean SpeedController::Turn(int degree, int direction){
+    motors.setEfforts(0, 0);
+    int turns = counts*(degree/180.0);
+    int count_turn = MagneticEncoder.ReadEncoderCountLeft();
+
+    while(abs(abs(count_turn) - abs(MagneticEncoder.ReadEncoderCountLeft())) <= turns){
+        if(!direction) Run(50,-50);
+        else Run(-50,50);
+    }
+    motors.setEfforts(0, 0);
+    return 1;
+}
+
+boolean SpeedController::Straight(int target_velocity, int time){
+    motors.setEfforts(0, 0);
+    unsigned long now = millis();
+
+    while ((unsigned long)(millis() - now) <= time*1000){
+        Run(target_velocity,target_velocity);
+    }
+    motors.setEfforts(0, 0);
+    return 1;
+}
+
+boolean SpeedController::Curved(int target_velocity_left, int target_velocity_right, int time) //in mm/s and s{
+    motors.setEfforts(0, 0);
+    unsigned long now = millis();
+
+    while ((unsigned long)(millis() - now) <= time*1000){
+        Run(target_velocity_left,target_velocity_right);
+    }
+    motors.setEfforts(0, 0);
+    return 1;
+}
+
+void SpeedController::Stop(){
+    motors.setEfforts(0,0);
+    odometry.Stop();
+    time_track = 0;
+}
+
+int constrainAceel(int targetSpeed){
+	float currSpeed = (MagneticEncoder.ReadVelocityLeft() + MagneticEncoder.ReadVelocityRight()) / 2; //can just read one, they should basically be the same value
+	int decVel = (int) (currSpeed + deltaV); //how much it can increase in speed by
+	int incVel = (int) (currSpeed - deltaV); //how much it can decrease in speed by
+	int vOut = constrain(targetSpeed, decVal, incVel);
+	return vOut;
+}
