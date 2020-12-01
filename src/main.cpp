7de57d1cc580d0572f68ba5c8942cecac337a7f3
mjhaahr <mjhaahr@wpi.edu>
2020-11-30 21:11:06 -0500

    Robot completes Parkour

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,16 @@
 #include <Arduino.h>
-//#include "Behaviors.h"
-#include "AccelControl.h"
+#include "Behaviors.h"
+//#include "AccelControl.h"
 
-//Behaviors followWaypoints;
-AccelControl accelControl;
+Behaviors followWaypoints;
+//AccelControl accelControl;
 
 void setup() {
-    //followWaypoints.Init();
-    accelControl.Init();
+    followWaypoints.Init();
+    //accelControl.Init();
 }
 
 void loop() {
-    //followWaypoints.Run();
-    accelControl.Run();
+    followWaypoints.Run();
+    //accelControl.Run();
 }
